e2e87e2a592285535c5847b458524b5850f29955
kisekinopureya <gokhanozdemir753@gmail.com>
2020-07-17 17:26:48 +0300

    Update anbox-binder.c

--- drivers/anbox/anbox-binder.c
+++ drivers/anbox/anbox-binder.c
@@ -57,12 +57,12 @@
 //#define BINDER_IPC_32BIT 1
 //#endif
 
-#define MAX_NICE 20
+//#define MAX_NICE 20
 
-static inline long rlimit_to_nice(long prio)
-{
-	return (MAX_NICE - prio + 1);
-}
+//static inline long rlimit_to_nice(long prio)
+//{
+//	return (MAX_NICE - prio + 1);
+//}
 
 /* Until I upstream a better version of this patch choose an arbitrary major
  * number in the high end spectrum that has not yet been given away and is
